278305b1bc69993ca503c7b5810da9f52d04ccc4
hazed0m <41749146+hazed0m@users.noreply.github.com>
2019-06-06 20:12:41 +0300

    final add captcha to Login/Registration

--- js/notifications.js
+++ js/notifications.js
@@ -1,3 +1,4 @@
+var emailValidate = false;
 function loginError(text)
 {
 	$('.alert-message').text(text);
@@ -32,16 +33,79 @@ $('.form-btn').on('click', function() {
         sendAccountInfos(parseInt($(this).attr('data-attr')));
     }
 });
-$('.form-input__wrap input').on('input',function(){
-    if($(this).parent().parent()[0].id === 'signin')
+$('input[type="text"]').keyup(function() {
+    if($(this)[0].id != 'newLoginEmail')
     {
-        $('.login').removeClass('disabled');
+        this.value = this.value.replace(/[^a-zA-Z0-9_]/g, '');
     }
-    if($(this).parent().parent()[0].id === 'signup')
+    else
     {
-        $('.register').removeClass('disabled');
-    }
- });
+        if(document.getElementById('newLoginEmail').value.match("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"))
+        {
+            emailValidate = true;
+        }
+        else
+        {
+            emailValidate = false;
+        }
+    }    
+});
+var verifyCallback = function(response) {
+    if(response)
+    {
+        $('.form-input__wrap input').keyup(function(){
+            if($(this).parent().parent()[0].id === 'signin')
+            {
+                if($('#loginName').val().length > 6 && $('#loginPass').val().length > 6)
+                {
+                    $('.login').removeClass('disabled');
+                }
+                else
+                {
+                    $('.login').addClass('disabled');
+                }
+            }
+            if($(this).parent().parent()[0].id === 'signup')
+            {
+                if($('#newLoginName').val().length > 6 && $('#newLoginPass').val().length > 6 && emailValidate)
+                {
+                    $('.register').removeClass('disabled');
+                }
+                else
+                {
+                    $('.register').addClass('disabled');
+                }
+            }           
+        });
+        if($('#loginName').val().length > 6 && $('#loginPass').val().length > 6)
+        {
+            $('.login').removeClass('disabled');
+        }
+        if($('#newLoginName').val().length > 6 && $('#newLoginPass').val().length > 6 && emailValidate)
+        {
+            $('.register').removeClass('disabled');
+        }
+    }    
+    else
+    {
+        alert('fail');
+    }     
+};
+
+var regCaptcha;
+var onloadCallback = function() {
+   
+    regCaptcha = grecaptcha.render('g-recaptcha1', {
+        'sitekey' : '6LcShqcUAAAAAEFfGkm9KKambio8Ik5XGbD3Lruz',
+        'callback' : verifyCallback,
+        'theme' : 'dark'
+    });
+    grecaptcha.render('g-recaptcha', {
+    'sitekey' : '6LcShqcUAAAAAEFfGkm9KKambio8Ik5XGbD3Lruz',
+    'callback' : verifyCallback,
+    'theme' : 'dark'
+    });
+};
 function sendAccountInfos(type) {
     switch (type) {
         case 0: 
