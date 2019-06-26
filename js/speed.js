575c97d80c3e65c39feafc3886e02ed6fa17f966
Graalex <41749146+hazed0m@users.noreply.github.com>
2019-06-26 20:40:34 +0300

    refresh speedmeter

--- js/speed.js
+++ js/speed.js
@@ -1,56 +1,70 @@
-
-let speed = new Vue({
-    el: '#speed',
-    data: {
-        speedActive: false,
-        vehicleArrowSpeed: 0,
-        vehicleSpeed: 0,
-        gear: '0',
-        gas: 89
-    },
-    methods: {
-        gasLines()
-        {
-            if(this.gas <= 10)
-            {
-                $('.energy-block').removeClass('active');
-            }
-            if(this.gas > 10 && this.gas <= 20)
-            {
-                $('.energy-block').removeClass('active');
-                $('.eb1').addClass('active');
-            }
-            if(this.gas > 20 && this.gas <= 35)
-            {
-                $('.energy-block').removeClass('active');
-                $('.eb1, .eb2').addClass('active');
-            }
-            if(this.gas > 35 && this.gas <= 45)
-            {
-                $('.energy-block').removeClass('active');
-                $('.eb1, .eb2, .eb3').addClass('active');
-            }
-            if(this.gas > 45 && this.gas <= 55)
-            {
-                $('.energy-block').removeClass('active');
-                $('.eb1, .eb2, .eb3, .eb4').addClass('active');
-            }
-            if(this.gas > 55 && this.gas <= 70)
-            {
-                $('.energy-block').removeClass('active');
-                $('.eb1, .eb2, .eb3, .eb4, .eb5').addClass('active');
-            }
-            if(this.gas > 70 && this.gas <= 85)
-            {
-                $('.energy-block').removeClass('active');
-                $('.eb1, .eb2, .eb3, .eb4, .eb5, .eb6').addClass('active');
-            }
-            if(this.gas > 85 && this.gas <= 100)
-            {
-                $('.energy-block').removeClass('active');
-                $('.eb1, .eb2, .eb3, .eb4, .eb5, .eb6, .eb7').addClass('active');
-            }
-        }
+function speedFadeIn()
+{
+    $('#speed').fadeIn();
+};
+function speedFadeOut()
+{
+    $('#speed').fadeOut();
+};
+function pushSpeed(vehicleArrowSpeed,vehicleSpeed,gear,gas)
+{
+    pushCurrentGear(gear);
+    pushCurrentArrow(vehicleArrowSpeed);
+    pushCurrentSpeed(vehicleSpeed);
+    gasLines(gas);
+};
+function pushCurrentGear(gear)
+{
+    $('.info-wrapper .gear').text(gear);
+}
+function pushCurrentArrow(vehicleArrowSpeed)
+{
+    $('.radius .arrow').css('transform',`rotateZ(${vehicleArrowSpeed}deg`);
+}
+function pushCurrentSpeed(vehicleSpeed)
+{
+    $('.info-wrapper .speed').text(vehicleSpeed);
+}
+function gasLines(gas)
+{
+    if(gas <= 10)
+    {
+        $('.energy-block').removeClass('active');
     }
-});
-speed.gasLines();
+    if(gas > 10 && gas <= 20)
+    {
+        $('.energy-block').removeClass('active');
+        $('.eb1').addClass('active');
+    }
+    if(gas > 20 && gas <= 35)
+    {
+        $('.energy-block').removeClass('active');
+        $('.eb1, .eb2').addClass('active');
+    }
+    if(gas > 35 && gas <= 45)
+    {
+        $('.energy-block').removeClass('active');
+        $('.eb1, .eb2, .eb3').addClass('active');
+    }
+    if(gas > 45 && gas <= 55)
+    {
+        $('.energy-block').removeClass('active');
+        $('.eb1, .eb2, .eb3, .eb4').addClass('active');
+    }
+    if(gas > 55 && gas <= 70)
+    {
+        $('.energy-block').removeClass('active');
+        $('.eb1, .eb2, .eb3, .eb4, .eb5').addClass('active');
+    }
+    if(gas > 70 && gas <= 85)
+    {
+        $('.energy-block').removeClass('active');
+        $('.eb1, .eb2, .eb3, .eb4, .eb5, .eb6').addClass('active');
+    }
+    if(gas > 85 && gas <= 100)
+    {
+        $('.energy-block').removeClass('active');
+        $('.eb1, .eb2, .eb3, .eb4, .eb5, .eb6, .eb7').addClass('active');
+    }    
+    $('.power-cont .energy-percent span').text(gas);
+};
