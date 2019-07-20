4e98bc4c62922c1531b6ddc3abb48169b7a64aea
Graalex <41749146+hazed0m@users.noreply.github.com>
2019-07-20 18:01:22 +0300

    carcircle + audio

--- js/carCircle.js
+++ js/carCircle.js
@@ -1,5 +1,10 @@
+var messageAudio = new Audio('audio/car.ogg');
 $('ul li.active').on('click',function(){
     let currentAction = $(this).find('.slot').attr('data-action');
+    if(currentAction == 'carAction')
+    {
+        messageAudio.play();
+    }
     mp.trigger(currentAction);   
 });
 
