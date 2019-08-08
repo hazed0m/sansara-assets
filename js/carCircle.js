e9248d6145db08d541d735e71945ff35e317fc85
Graalex <41749146+hazed0m@users.noreply.github.com>
2019-08-08 23:07:16 +0300

    refresh sound + new item carCircle

--- js/carCircle.js
+++ js/carCircle.js
@@ -1,9 +1,14 @@
-var messageAudio = new Audio('audio/car.ogg');
+var messageAudio = new Audio('audio/signal.ogg'),
+    signalAudio = new Audio('audio/signal1.ogg');
 $('ul li.active').on('click',function(){
     let currentAction = $(this).find('.slot').attr('data-action');
     if(currentAction == 'carAction')
     {
         messageAudio.play();
+    }
+    if(currentAction == 'parking')
+    {
+        signalAudio.play();
     }
     mp.trigger(currentAction);   
 });
