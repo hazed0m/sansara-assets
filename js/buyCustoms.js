ce04ab3b27de95e9e79b29b71f3db2f60eee5d2f
hazed0m <hazekk94@gmail.com>
2019-12-02 18:59:51 +0200

    append BuyCustoms

---
+++ js/buyCustoms.js
@@ -0,0 +1,41 @@
+var customsName = '',
+	customsPrice = 0;
+$('.cash, .card').on('click',function(){
+	$(this).each(function(index, item){
+		if($(item).hasClass('active'))
+		{
+			$(item).removeClass('active');			
+			$(item).find('i').removeClass('fas').addClass('far');
+		}
+		else
+		{
+			$('.cash, .card').each(function(index, item){
+				if($(item).hasClass('active'))
+				{
+					$(item).removeClass('active');			
+					$(item).find('i').removeClass('fas').addClass('far');
+				}
+			});
+			$(item).addClass('active');
+			$(item).find('i').removeClass('far').addClass('fas');
+		}		
+	});
+});
+$('#buy').on('click',function(){
+	let cashService = $('.radio-block .active')[0].classList[0];
+	customsName = $('input.stoName').val();
+	if(customsName.length != 0)
+	{
+		$('input.stoName').val('');
+		console.log(cashService, customsName, customsPrice);
+		mp.trigger("buyCustomsButton", cashService, customsName, customsPrice);
+	}
+});
+$('.close-but').on('click',function(){
+	mp.trigger("buyCustomsExit");
+});
+function pushBuyCustoms(customsprice)
+{
+	customsPrice = customsprice;
+	$('.price-block .sell-price').text(customsPrice);
+};
