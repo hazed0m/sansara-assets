f6baa58a5792c9e5aeea4288597365fee4a80222
hazed0m <hazekk94@gmail.com>
2019-12-06 15:06:07 +0200

    rfr operationMenu + diagnosis popup personalCard

--- js/personalCard.js
+++ js/personalCard.js
@@ -12,8 +12,33 @@ function pushPersonalCard(elem)
         });
     });
 }
-$('.container .wrapper .inner-wrapper .button').on('click',function(){
+$('.container .wrapper .button').on('click',function(){
     let id = this.id;
     console.log(id);
-    mp.trigger(id);
+    if(id == 'diagnosis')
+    {
+        $('.container .popup-wrapper').fadeIn();
+    }
+    else if(id == 'sendDiagnosis')
+    {
+        let text = $(this).prev().val();
+        if(text.length != 0)
+        {
+            $(this).parent().fadeOut();
+            $(this).prev().val('');
+            
+            console.log(id,text);
+            mp.trigger(id,text);
+        }
+    }
+    else
+    {
+        mp.trigger(id);
+    }
+});
+$('textarea').keyup(function() {
+	this.value = this.value.replace(/[^a-zA-Zа-яА-Я0-9,.!?_ ]/g, '');
+});
+$('.container .popup-wrapper .close-but').on('click',function(){
+    $('.container .popup-wrapper').fadeOut();
 });
