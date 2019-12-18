e72b6ac073610352ca3f7a16314184bc7c217e3f
hazed0m <hazekk94@gmail.com>
2019-12-18 18:56:50 +0200

    add newComa

--- js/coma.js
+++
@@ -1,5 +0,0 @@
-$('.container').fadeIn(1000).css('display','flex');
-function comaFadeOut(){
-	$('.container').fadeOut(1000);
-	mp.trigger('comaScreenOff');
-};
---
+++ js/comaTimer.js
@@ -0,0 +1,71 @@
+let counterInterval = setInterval(function(){
+    let counterNumber = getCounterNumber();
+        if(counterNumber != 0)
+        {
+            $('.counter-wrapper .number').text(counterNumber -= 1);
+        }
+        else
+        {                  
+            if($('.counter-wrapper .number').attr('counter') == 'button')
+            {
+                clearInterval(counterInterval);
+                $('.button-wrapper .button').addClass('disabled');
+                mp.trigger('comaOutOfTime');
+            } 
+            if($('.counter-wrapper .number').attr('counter') == 'wait')
+            {
+                $('.text-wrapper, .counter-wrapper').fadeOut();
+                setTimeout(function(){
+                    $('.counter-wrapper .number').text(10);
+                    $('.coma-wrapper, .counter-wrapper').fadeIn();
+                    $('.counter-wrapper .number').attr('counter','button');
+                },500);
+            }
+        }
+    },1000);
+function runTimer(id)
+{
+    if(id == 'waitAmbulance')
+    {
+        $('.coma-wrapper, .counter-wrapper').fadeOut();
+        $('.counter-wrapper .number').attr('counter','wait');
+        setTimeout(function(){
+            $('.counter-wrapper .number').text(10);
+            $('.text-wrapper, .counter-wrapper').fadeIn();
+        },500);
+    }
+    if(id == 'toHospital')
+    {
+        clearInterval(counterInterval);
+        $('.button-wrapper .button').addClass('disabled');
+    }
+}
+function pushSyndrome(text)
+{
+    $('.text-wrapper .text span').text(text);
+}
+function getCounterNumber()
+{
+    return parseInt($('.counter-wrapper .number').text());
+}
+function comaFadeIn()
+{
+    clearInterval(counterInterval);
+    $('.coma-wrapper, .counter-wrapper').fadeOut();
+    setTimeout(function(){
+        $('.container').css({'background-color':'rgba(0,0,0,0.95)'});
+        $('.death-wrapper').fadeIn();
+    },500);
+}
+function comaFadeOut(){
+	$('.container').fadeOut(1000);
+	mp.trigger('comaScreenOff');
+};
+$('.button-wrapper .button').on('click',function(){
+    let id = this.id;
+    console.log(id);
+    if(!$(this).hasClass('disabled'))
+    {
+        mp.trigger(id);
+    }
+});
