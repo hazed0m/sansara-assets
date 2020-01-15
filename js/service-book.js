2cd1af5375660c3711a620ea75e726a19e429781
hazed0m <hazekk94@gmail.com>
2020-01-15 12:53:44 +0200

    rfr

--- js/service-book.js
+++ js/service-book.js
@@ -28,15 +28,17 @@ let service = JSON.stringify({
     carType: 'Arnold',
     carScore: 25252,
     dateOf: '25.11.2019',
+    tuningInfo: 'asfsasaffasfafsa@фыафафафаыфы',
     doneWorks: 'Здесь должен быть текст сто процентов Вас уверяю.@Здесь должен быть текст сто процентов Вас уверяю. Здесь должен быть текст сто процентов Вас уверяю. @Здесь должен быть текст сто процентов Вас уверяю. Здесь должен быть текст сто процентов Вас уверяю. @Здесь должен быть текст сто процентов Вас уверяю. Здесь должен быть текст сто процентов Вас уверяю. @Здесь должен быть текст сто процентов говорю.'
 });
 function pushServiceBook(elem)
 {
     serviceBook = JSON.parse(elem);
-    let serviceWorks = serviceBook.doneWorks.split('@'); 
+    let serviceWorks = serviceBook.doneWorks.split('@'),
+        tuningInfo = serviceBook.tuningInfo.split('@');
     for (const elem in serviceBook)
     {
-        if(elem != 'doneWorks')
+        if(elem != 'doneWorks' && elem != 'tuningInfo')
         {
             console.log(`.container .page #${elem} .text-block`);
             $(`.container .page #${elem} .text-block`).append(serviceBook[elem]);
@@ -44,5 +46,8 @@ function pushServiceBook(elem)
     }
     $(serviceWorks).each(function(index,item){
         $(`.container .page #doneWorks .text-block`).append(`<p>${item}</p>`)
+    });
+    $(tuningInfo).each(function(index,item){
+        $(`.container .page #tuningInfo .text-block`).append(`<p>${item}</p>`)
     });
 }
