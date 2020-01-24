096dfd8de6bb7f59053493140faef583af264356
hazed0m <hazekk94@gmail.com>
2020-01-24 13:51:11 +0200

    rfr medicalWardrobe

---
+++ js/medicalWardrobe.js
@@ -0,0 +1,341 @@
+let wardrobeMaleSetList = [
+        {
+            name:'Комплект МЕД м 1',
+            clothes: [
+                'Куртка м спортивная 4',
+                'Торс м мед перчатки 5',
+                'Аксессуар м стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД м 2',
+            clothes: [
+                'Куртка м спортивная 4',
+                'Торс м мед перчатки 5',
+                'Аксессуар м бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД м 3',
+            clothes: [
+                'Куртка м спортивная 4',
+                'Торс м мед перчатки 5',
+                'Аксессуар м стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД м 4',
+            clothes: [
+                'Куртка м спортивная 4',
+                'Торс м мед перчатки 5',
+                'Аксессуар м бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД м 5',
+            clothes: [
+                'Куртка м рубашка с короткими рукавами 1',
+                'Торс м мед перчатки',
+                'Аксессуар м стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД м 6',
+            clothes: [
+                'Куртка м рубашка с короткими рукавами 1',
+                'Торс м мед перчатки',
+                'Аксессуар м бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД м 7',
+            clothes: [
+                'Куртка м рубашка с короткими рукавами 1',
+                'Торс м мед перчатки',
+                'Аксессуар м стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД м 8',
+            clothes: [
+                'Куртка м рубашка с короткими рукавами 1',
+                'Торс м мед перчатки',
+                'Аксессуар м бейдж'
+            ]
+        }
+    ],
+    wardrobeMaleClothesList = [				
+        // 'Торс м мед перчатки',
+        // 'Торс м мед перчатки 5',
+        'Аксессуар м стетоскоп',
+        'Аксессуар м бейдж',
+        'Очки м респератор 26',
+        'Шляпа м мед кепка',
+        'Значки м медик',
+        'Значки м медик 1',
+        'Низ м брюки 5',
+        'Низ м узкие брюки'
+    ],
+    wardrobeFemaleSetList = [
+        {
+            name:'Комплект МЕД ж 1',
+            clothes: [
+                'Куртка ж майка 6',
+                'Торс ж мед перчатки 11',
+                'Аксессуар ж стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 2',
+            clothes: [
+                'Куртка ж майка 6',
+                'Торс ж мед перчатки 11',
+                'Аксессуар ж бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 3',
+            clothes: [
+                'Куртка ж худи',
+                'Торс ж мед перчатки 3',
+                'Аксессуар ж стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 4',
+            clothes: [
+                'Куртка ж худи',
+                'Торс ж мед перчатки 3',
+                'Аксессуар ж бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 5',
+            clothes: [
+                'Куртка ж худи',
+                'Торс ж мед перчатки 3',
+                'Аксессуар ж стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 6',
+            clothes: [
+                'Куртка ж худи',
+                'Торс ж мед перчатки 3',
+                'Аксессуар ж бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 7',
+            clothes: [
+                'Куртка ж поло 3',
+                'Торс ж мед перчатки 11',
+                'Аксессуар ж стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 8',
+            clothes: [
+                'Куртка ж поло 3',
+                'Торс ж мед перчатки 11',
+                'Аксессуар ж стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 9',
+            clothes: [
+                'Куртка ж поло 3',
+                'Торс ж мед перчатки 11',
+                'Аксессуар ж стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 10',
+            clothes: [
+                'Куртка ж поло 3',
+                'Торс ж мед перчатки 11',
+                'Аксессуар ж бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 11',
+            clothes: [
+                'Куртка ж поло 3',
+                'Торс ж мед перчатки 12',
+                'Аксессуар ж бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 12',
+            clothes: [
+                'Куртка ж поло 3',
+                'Торс ж мед перчатки 13',
+                'Аксессуар ж бейдж'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 13',
+            clothes: [
+                'Куртка ж джинсовая',
+                'Торс ж мед перчатки 1',
+                'Аксессуар ж стетоскоп'
+            ]
+        },
+        {
+            name:'Комплект МЕД ж 14',
+            clothes: [
+                'Куртка ж джинсовая',
+                'Торс ж мед перчатки 1',
+                'Аксессуар ж бейдж'
+            ]
+        }
+    ],
+    wardrobeFemaleClothesList = [		
+        // 'Торс ж мед перчатки',
+        // 'Торс ж мед перчатки 1',
+        // 'Торс ж мед перчатки 2',
+        // 'Торс ж мед перчатки 3',
+        // 'Торс ж мед перчатки 4',
+        // 'Торс ж мед перчатки 5',
+        // 'Торс ж мед перчатки 6',
+        // 'Торс ж мед перчатки 7',
+        // 'Торс ж мед перчатки 8',
+        // 'Торс ж мед перчатки 9',
+        // 'Торс ж мед перчатки 10',
+        // 'Торс ж мед перчатки 11',
+        // 'Торс ж мед перчатки 12',
+        // 'Торс ж мед перчатки 13',
+        // 'Торс ж мед перчатки 14',
+        // 'Торс ж мед перчатки 15',
+        // 'Торс ж мед перчатки 16',
+        // 'Торс ж мед перчатки 17',
+        'Аксессуар ж стетоскоп',
+        'Аксессуар ж бейдж',
+        'Очки ж респератор 28',
+        'Шляпа ж мед кепка',
+        'Значки ж медик',
+        'Значки ж медик 1',
+        'Низ ж юбка карандаш 1',
+        'Низ ж брюки со стрелками 2'
+    ],
+    wardrobeNames = [
+        'Торс',
+        'Низ',
+        'Аксессуар',
+        'Шляпа',
+        'Бронежилет',
+        'Значки',
+        'Очки'
+    ],
+    wardrobeNamesTranslated = [
+        'Top',
+        'Bottom',
+        'Accessories',
+        'Hat',
+        'Armor',
+        'Badges',
+        'Glasses'
+    ];
+$('.container .wardrobe-wrapper .wardrobe-category-title').on('click',function(){
+    if(!$(this).next().is(':visible'))
+    {
+        $(this).next().slideDown();
+        $(this).find('svg').css({
+            'transform':'rotate(180deg)'
+        });
+    }
+    else
+    {
+        $(this).next().slideUp();
+        $(this).find('svg').css({
+            'transform':'rotate(360deg)'
+        });
+    }
+});
+function pushWardrobe(sex)
+{
+    if(sex == 'man')
+    {
+        sex = 'Male';
+    }
+    else if(sex == 'girl')
+    {
+        sex = 'Female'
+    }
+    $('.container .wardrobe-wrapper #Set').empty();
+    $(eval(`wardrobe${sex}SetList`)).each(function(index,item){
+        let template = `
+            <div class="wardrobe-item" id="${sex}" data-index="${index}" data-list="wardrobe${sex}SetList">
+                <div class="wardrobe-name">${item.name}</div>
+                <img src="images/inventory/Clothes_Duty.png" alt="">
+            </div>`;
+        $('.container .wardrobe-wrapper #Set').append(template);
+    });
+    $(eval(`wardrobe${sex}ClothesList`)).each(function(index,item){
+        $(wardrobeNames).each(function(indexEl,classEl){
+            if(item.includes(classEl))
+            {
+                let template = `
+                    <div class="wardrobe-item" id="${sex}" data-index="${index}" data-list="wardrobe${sex}ClothesList">
+                        <div class="wardrobe-name">${item}</div>
+                        <img src="images/person/${wardrobeNamesTranslated[indexEl]}.png" alt="">
+                    </div>`;
+                $(`.container .wardrobe-wrapper #${wardrobeNamesTranslated[indexEl]}`).append(template);
+            }
+        });
+    });
+    initWardrobe();
+}
+function initWardrobe()
+{
+    $('.wardrobe-item').on('click',function(){
+        if(!$(this).hasClass('closed'))
+        {   
+            let id = $(this).parent()[0].id;
+            if($(this).hasClass('active'))
+            {
+                $(this).removeClass('active');
+                $('.button#use').addClass('disabled');
+            }
+            else
+            {
+                $('.button#use').removeClass('disabled');
+                $(`#${id} .wardrobe-item.active`).removeClass('active');
+                $(this).addClass('active');
+            }
+        }
+    });
+    $('.button#use').on('click',function(){
+        if($('.wardrobe-item.active').length != 0)
+        {
+            let activeList = [];
+            $('.wardrobe-item.active').each(function(index,item){
+                let element = null;                
+                element = $(eval($(item).attr('data-list')));
+                if($(this).parent()[0].id != 'Set')
+                {
+                    element = { name : element[$(item).attr('data-index')], clothes : [ element[$(item).attr('data-index')] ]};
+                }
+                else
+                {
+                    element = eval($(item).attr('data-list'))[$(item).attr('data-index')];
+                    console.log('Set',element);
+                }
+                console.log(element);
+                activeList.push(element);
+            });      
+            console.log(activeList);
+            $('.button#use').addClass('disabled');
+            $('.wardrobe-wrapper .active').removeClass('active');
+            $('.container .wardrobe-category-title').next().slideUp();
+            $('.container .wardrobe-category-title').find('svg').css({
+                'transform':'rotate(360deg)'
+            });
+            mp.trigger('MCUseWardrobe',JSON.stringify(activeList));
+        }
+    });
+    $('.button#close').on('click',function(){
+        mp.trigger('closeMedicalWardrobe');
+    });
+    
+}
