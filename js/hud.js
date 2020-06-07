28fbbd92f6cd8ef68dbc556106f40c0dff04ef21
Graalex <41749146+hazed0m@users.noreply.github.com>
2020-06-07 17:04:50 +0300

    upd hud hover

--- js/hud.js
+++ js/hud.js
@@ -45,12 +45,12 @@ $('.button#hover').on('click',function(){
     if($('.hud-footer__wrap').is(':visible'))
     {
         $(this).css('bottom','-16px');
-        $('.hud-footer__wrap, .geo-wrapper, .buttons').slideUp();
+        $('.hud-footer__wrap, .buttons').slideUp();
         $(this).find('i').css('transform','rotate(180deg)');
     }
     else
     {
-        $('.hud-footer__wrap, .geo-wrapper, .buttons').slideDown();
+        $('.hud-footer__wrap, .buttons').slideDown();
         $(this).find('i').css('transform','rotate(0deg)');
         $(this).css('bottom','-17px');
     }
